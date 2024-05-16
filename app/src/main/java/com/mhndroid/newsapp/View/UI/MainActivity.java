f8957342ab39bf61dd88092bcbabc220a5a147cc
Muhammad Hussain <imhussain9014@gmail.com>
2024-05-16 10:38:33 +0600

    Add Theme and main display Screen

---
+++ app/src/main/java/com/mhndroid/newsapp/View/UI/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mhndroid.newsapp.View.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mhndroid.newsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
