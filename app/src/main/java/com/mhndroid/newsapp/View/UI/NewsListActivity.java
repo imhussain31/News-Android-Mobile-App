cfb960dcfd6ac67c667290f058d6eda228d217f5
Muhammad Hussain <imhussain9014@gmail.com>
2024-05-16 11:03:51 +0600

    Add NewsListActivity and single row design for each list item

---
+++ app/src/main/java/com/mhndroid/newsapp/View/UI/NewsListActivity.java
@@ -0,0 +1,16 @@
+package com.mhndroid.newsapp.View.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mhndroid.newsapp.R;
+
+public class NewsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_list);
+    }
+}
